63c9a740efd91c9ae3828da0483728784f365263
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-28 11:55:45 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 radio.onReceivedValue(function (name, value) {
     serial.writeValue(name, value)
     led.toggle(2, 0)
+    if (name == "step") {
+        OLED.writeNumNewLine(value)
+    }
 })
+OLED.init(128, 64)
 basic.showLeds(`
     # . . . #
     . # . # .
