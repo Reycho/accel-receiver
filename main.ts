a9919213ac557e45483776bde5ff811515b9a4f4
Reycho <62747204+Reycho@users.noreply.github.com>
2022-06-28 14:55:43 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
 radio.onReceivedValue(function (name, value) {
-    serial.writeValue(name, value)
     led.toggle(2, 0)
     if (name == "step") {
         OLED.writeNumNewLine(value)
+        if (value % 2 == 0) {
+            serial.writeValue("a.steps", 2000)
+        } else {
+            serial.writeValue("a.steps", 0)
+        }
+        serial.writeValue("steps", value)
+    } else {
+        serial.writeValue("a.accel", value)
     }
 })
 OLED.init(128, 64)
